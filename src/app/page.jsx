31d6e5b21387606a20807155b88b5d09c190e3b8
Deepka26 <deepka1000@gmail.com>
2024-07-25 23:18:15 +1200

    conflict resolved

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,25 +15,17 @@ import TrialCard from "@/components/Cards/TrialCard";
 import Updates from "@/components/Home/Updates";
 import {auth} from "@/auth";
 
-<<<<<<< HEAD
 const Home = async() => {
  const session=await auth();
-=======
-const Home = () => {
->>>>>>> d2a73fc59bc64d5474bb022daa9106b147ef1e2b
  
   return (
     <>
       <MainPoster />
 
-<<<<<<< HEAD
   <Updates session={session}/>
   
 
      
-=======
-      <Updates/>
->>>>>>> d2a73fc59bc64d5474bb022daa9106b147ef1e2b
 
       <UserCount />
       <Information />
